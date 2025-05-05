f38fc38dc38816b273e5023e2198eead0f1c3013
apostolis1 <apostolis.stamatis1@gmail.com>
2025-05-05 10:43:48 +0300

    Adds Distributed Implementations for Lustre kernels

--- src/runtime/distributed/coordinator/kernels/DistributedRead.h
+++ src/runtime/distributed/coordinator/kernels/DistributedRead.h
@@ -86,7 +86,10 @@ template <class DTRes> struct DistributedRead<ALLOCATION_TYPE::DIST_GRPC_ASYNC, 
 
 template <class DTRes> struct DistributedRead<ALLOCATION_TYPE::DIST_GRPC_SYNC, DTRes> {
     static void apply(DTRes *&res, const char *filename, DCTX(dctx)) {
+        std::filesystem::path filePath(filename);
+        auto extension = filePath.extension().string();
 #if USE_HDFS
+    if (extension == ".hdfs") {
         auto ctx = DistributedContext::get(dctx);
         auto workers = ctx->getWorkers();
 
@@ -129,6 +132,55 @@ template <class DTRes> struct DistributedRead<ALLOCATION_TYPE::DIST_GRPC_SYNC, D
 
         for (auto &thread : threads_vector)
             thread.join();
+    }
 #endif
+        // TODO Make this work when HDFS defined, probably split the functions up and use the file extention or something to decide
+        // whether we have lustre or HDFS, or with the compile flag --lustre
+    if (extension == ".lustre") {
+        auto ctx = DistributedContext::get(dctx);
+        auto workers = ctx->getWorkers();
+
+        std::vector<std::thread> threads_vector;
+        LoadPartitioningDistributed<DTRes, AllocationDescriptorGRPC> partioner(
+            DistributionSchema::DISTRIBUTE, res, dctx);
+        
+        while (partioner.HasNextChunk()) {
+            auto lustreFn = std::string(filename);
+            auto dp = partioner.GetNextChunk();
+
+            auto workerAddr =
+                dynamic_cast<AllocationDescriptorGRPC *>(dp->allocation.get())
+                    ->getLocation();
+             std::thread t([=, &res]() {
+                auto stub = ctx->stubs[workerAddr].get();
+
+                distributed::LustreFile fileData;
+                fileData.set_filename(lustreFn);
+                fileData.set_start_row(dp->range->r_start);
+                fileData.set_num_rows(dp->range->r_len);
+                fileData.set_num_cols(dp->range->c_len);
+
+                grpc::ClientContext grpc_ctx;
+                distributed::StoredData response;
+
+                auto status = stub->ReadLustre(&grpc_ctx, fileData, &response);
+                if (!status.ok()) {
+                    std::cout << "Status not ok\n";
+                    throw std::runtime_error(status.error_message());
+                }
+                DistributedData newData;
+                newData.identifier = response.identifier();
+                newData.numRows = response.num_rows();
+                newData.numCols = response.num_cols();
+                newData.isPlacedAtWorker = true;
+                dynamic_cast<AllocationDescriptorGRPC &>(*(dp->allocation))
+                    .updateDistributedData(newData);
+            });
+            threads_vector.push_back(move(t));
+        }
+
+        for (auto &thread : threads_vector)
+            thread.join();
+    }
     }
 };
--- src/runtime/distributed/coordinator/kernels/DistributedWrite.h
+++ src/runtime/distributed/coordinator/kernels/DistributedWrite.h
@@ -25,6 +25,10 @@
 #include <runtime/distributed/worker/WorkerImpl.h>
 #include <runtime/local/datastructures/AllocationDescriptorGRPC.h>
 
+#include <runtime/local/io/lustre/WriteLustreCsv.h>
+#include <runtime/local/io/lustre/WriteDaphneLustre.h>
+#include <runtime/local/io/DaphneSerializer.h>
+
 #ifdef USE_MPI
 #include <runtime/distributed/worker/MPIHelper.h>
 #endif
@@ -85,10 +89,14 @@ template <class DTArg> struct DistributedWrite<ALLOCATION_TYPE::DIST_GRPC_ASYNC,
 // ----------------------------------------------------------------------------
 // Synchronous GRPC
 // ----------------------------------------------------------------------------
-#ifdef USE_HDFS
 
 template <class DTArg> struct DistributedWrite<ALLOCATION_TYPE::DIST_GRPC_SYNC, DTArg> {
     static void apply(const DTArg *mat, const char *filename, DCTX(dctx)) {
+
+        std::filesystem::path filePath(filename);
+        auto extension = filePath.extension().string();
+#ifdef USE_HDFS
+        if (extension == ".hdfs") {
         auto ctx = DistributedContext::get(dctx);
         auto workers = ctx->getWorkers();
 
@@ -157,5 +165,126 @@ template <class DTArg> struct DistributedWrite<ALLOCATION_TYPE::DIST_GRPC_SYNC, 
         for (auto &thread : threads_vector)
             thread.join();
     }
-};
 #endif
+        if (extension == ".lustre") {
+            auto ctx = DistributedContext::get(dctx);
+            auto workers = ctx->getWorkers();
+
+            if (mat == nullptr) {
+                throw std::runtime_error("matrix argument is null");
+            }
+            
+            std::filesystem::path filePath(filename);
+
+            // Get nested file extension
+            auto extension = filePath.stem().extension().string();
+            // The coordinator should create the file, so each worker writes to the existing file at the specified offset
+            std::string fn(filename);
+            
+            // TODO Check if path exists
+            // Write file metadata
+            FileMetaData fmd(mat->getNumRows(), mat->getNumCols(), true, ValueTypeUtils::codeFor<double>);
+            auto fmdStr = MetaDataParser::writeMetaDataToString(fmd);
+            if (fmdStr.size() == 0 ) {
+                throw std::runtime_error("Metadata string could not be parsed\n");
+                return ;
+            }
+            auto mdtFn = fn + ".meta";
+
+            // TODO what if the file already exists
+
+            // Delete files if they exist TODO: How do we handle the case when file already exists?
+            if (LUSTRE_DELETE_FILES_IF_EXIST) {
+                if (std::filesystem::remove(static_cast<const char *>(mdtFn.c_str())))
+                    std::cout << "Removed file " << mdtFn << std::endl;
+                if (std::filesystem::remove(filename))
+                    std::cout << "Removed file " << filename << std::endl;
+            }
+            
+            // Open metadata file for writing
+            int fd = LustreUtils::openMetadataFile(static_cast<const char *>(mdtFn.c_str()), O_CREAT | O_WRONLY);
+            if (fd < 0)
+                throw std::runtime_error("Error opening Metadata file");
+
+            // Write metadata
+            
+            dprintf(fd, "%s", fmdStr.c_str());
+            LustreUtils::closeFile(fd);
+            // Create .lustre file
+            // If this is a daphne object file, the coordinator should write the header because it has the overview of the whole matrix,
+            // thus it can create the header. The workers should write the data only
+            fd = LustreUtils::openFile(static_cast<const char *>(fn.c_str()), O_CREAT | O_WRONLY, dctx);
+            if (fd < 0)
+                throw std::runtime_error("Error opening Lustre file");
+            // In case of daphne object file the coordinator must  
+            if (extension == ".dbdf") {
+                size_t headerSize = DaphneSerializer<DTArg>::headerSize(mat);
+                std::vector<char> buffer(headerSize);
+                DaphneSerializer<DTArg>::serializeHeader(mat, buffer.data());
+                // The header is always written at the beginning of the file
+                pwrite(fd, buffer.data(), headerSize, 0);
+            }
+            LustreUtils::closeFile(fd);
+            
+            std::vector<std::thread> threads_vector;
+            for (auto workerAddr : workers) {
+                DataPlacement *dp;
+                if ((dp = mat->getMetaDataObject()->getDataPlacementByLocation(
+                        workerAddr))) {
+                    auto data =
+                        dynamic_cast<AllocationDescriptorGRPC &>(*(dp->allocation))
+                            .getDistributedData();
+                    if (data.isPlacedAtWorker) {
+                        // std::cout << "Data placed at worker\n";
+                        // std::cout << "r_start: " << dp->range.get()->r_start << " r_len: " << dp->range.get()->r_len << std::endl;
+                        std::thread t([=, &mat]() {
+                            auto stub = ctx->stubs[workerAddr].get();
+
+                            distributed::LustreWriteInfo fileData;
+                            fileData.mutable_matrix()->set_identifier(
+                                data.identifier);
+                            fileData.mutable_matrix()->set_num_rows(data.numRows);
+                            fileData.mutable_matrix()->set_num_cols(data.numCols);
+                            
+                            fileData.set_filename(filename);
+                            fileData.set_start_row(dp->range.get()->r_start);
+                            grpc::ClientContext grpc_ctx;
+                            distributed::Empty empty;
+
+                            auto status =
+                                stub->WriteLustre(&grpc_ctx, fileData, &empty);
+                            if (!status.ok())
+                                throw std::runtime_error(status.error_message());
+                        });
+                        threads_vector.push_back(move(t));
+                    } else {
+                        auto slicedMat = mat->sliceRow(dp->range.get()->r_start,
+                                                    dp->range.get()->r_start +
+                                                        dp->range.get()->r_len);
+                        // TODO: This hasn't been tested
+                        if (extension == ".csv") {
+                            writeLustreCsv(slicedMat, filename, dctx);
+                            }
+                        else if (extension == ".dbdf") {
+                            writeDaphneLustre(slicedMat, filename, dctx);
+                        }
+                    }
+                } else {
+                    std::cout << "No dp placement, this will be skipped" << std::endl;
+                    // if (extension == ".csv") {
+                    //     writeLustreCsv(mat, filename, dctx);
+                    //     }
+                    // else if (extension == ".dbdf") {
+                    //     writeDaphneLustre(mat, filename, dctx);
+                    // }
+                    continue;
+                }
+                // TODO we should also store ranges that did not have a
+                // dataplacement associated with them
+            }
+            for (auto &thread : threads_vector)
+                thread.join();
+
+        }
+    }
+};
